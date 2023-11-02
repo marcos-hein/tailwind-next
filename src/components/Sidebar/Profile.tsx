9a249e83e066f8d8885ec05912c4c67932928e6c
Marcos Pereira <vinicius.hein@gmail.com>
2023-11-02 19:31:10 -0300

    feat: creating input component with composition pattern

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,23 @@
+import { LogOut } from "lucide-react";
+
+export function Profile() {
+  return (
+    <div className="flex items-center gap-3">
+      <img
+        src="https://github.com/marcos-hein.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">Marcos Hein</span>
+        <span className="truncate text-sm text-zinc-500">
+          vinicius.hein@gmail.com
+        </span>
+      </div>
+
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  );
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,52 @@
-import { Search } from "lucide-react";
+import {
+  BarChart2,
+  CheckSquare,
+  Flag,
+  Home,
+  Layers3,
+  LifeBuoy,
+  Search,
+  Settings,
+  Users,
+} from "lucide-react";
 import { Logo } from "./Logo";
-import { MainNavigation } from "./MainNavigation";
+import { NavItem } from "./NavItem";
+import { UsedSpaceWidget } from "./UsedSpaceWidget";
+import { Profile } from "./Profile";
+import { Input } from "../Input";
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
-      <MainNavigation />
+      <nav className="space-y-1">
+        <NavItem icon={Home} title="Home" />
+        <NavItem icon={BarChart2} title="Dashboard" />
+        <NavItem icon={Layers3} title="Projects" />
+        <NavItem icon={CheckSquare} title="Tasks" />
+        <NavItem icon={Flag} title="Reporting" />
+        <NavItem icon={Users} title="Users" />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-1">
+          <NavItem icon={LifeBuoy} title="Support" />
+          <NavItem icon={Settings} title="Settings" />
+        </nav>
+
+        <UsedSpaceWidget />
+
+        <div className="h-px w-full border border-zinc-200" />
+        <Profile />
+      </div>
     </aside>
   );
 }

f92baaf904b6b59a43f932e48d630fcdaab469b5
Marcos Pereira <vinicius.hein@gmail.com>
2023-11-19 18:02:50 -0300

    feat: file listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,8 +142,9 @@ export default function Home() {
                 Share a few snippets of your work.
               </span>
             </label>
-            <FileInput.Root className="flex items-start gap-5">
+            <FileInput.Root>
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control multiple />
             </FileInput.Root>
           </div>
--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -7,7 +7,7 @@ export type RootProps = React.ComponentProps<'div'>
 type FileInputContextType = {
   id: string
   files: File[]
-  onFilesSelected: (files: File[]) => void
+  onFilesSelected: (files: File[], mutiple: boolean) => void
 }
 
 const FileInputContext = createContext<FileInputContextType>(
@@ -18,8 +18,16 @@ export function Root(props: RootProps) {
   const id = useId()
   const [files, setFiles] = useState<File[]>([])
 
+  function onFilesSelected(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((prevFiles) => [...prevFiles, ...files])
+      return
+    }
+    setFiles(files)
+  }
+
   return (
-    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
+    <FileInputContext.Provider value={{ id, files, onFilesSelected }}>
       <div {...props} />
     </FileInputContext.Provider>
   )

0c558a53eb72c15dd84492789ea58535fe19c367
Marcos Pereira <vinicius.hein@gmail.com>
2023-11-21 20:35:40 -0300

    feat: creates button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import * as FileInput from '../components/Form/FileInput'
 import { SelectItem } from '../components/Form/Select/SelectItem'
 import { Select } from '../components/Form/Select'
 import { Textarea } from '../components/Form/Textarea'
+import { Button } from '../components/Button'
 
 export default function Home() {
   return (
@@ -22,19 +23,8 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg border border-zinc-300 bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
-              Save
-            </button>
+            <Button variant="outline">Cancel</Button>
+            <Button variant="primary">Save</Button>
           </div>
         </div>
 
@@ -214,18 +204,8 @@ export default function Home() {
           </div>
 
           <div className="flex justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-lg border border-zinc-300 bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
-              Save
-            </button>
+            <Button variant="outline">Cancel</Button>
+            <Button variant="primary">Save</Button>
           </div>
         </form>
       </div>
